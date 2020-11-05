e12be9a9509d3af84cdd6d7350672eaa29fb8876
ankankankanka <73925669+ankankankanka@users.noreply.github.com>
2020-11-05 10:03:58 +0800

    代码
    代码

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-//演示golang中基本数据类型的转换
-func main () {
-	var i  = 100
-	//希望将i => float64
-	var n1  = float32(i)
-	var n2 = int8(i)
-	var n3 = int64(i)//低精度->高精度
+func main() {
+	//golang的变量使用方式1
+	//第一种，指定变量类型，声明后若不赋值，使用默认值
+	//int 的默认值就是0
+	var i int
+	fmt.Println("i=",i)
+	//第二种，根据值自行判定变量类型（类型推导）
+	var num = 10.11
+	fmt.Println("num=", num )
+	//第三种，省略var，注意；=左侧的变量不应该时已经声明过的否则会导致编译错误
+	//：=的 ：不能省略，否则错误
+	name :="tom"
+	fmt.Println("name=",name)
 
-	fmt.Printf("i=%v n1=%v n2=%v n3=%v",i,n1,n2,n3)
 
-	//在转换中，比如将 int64 转成 int8 【-128---127】，编译时不会报错
-	//只是转换的结果是按溢出处理，和我们希望的结果不一样
-	var num1 int64 =99999
-	var num2  = int8(num1)
-	fmt.Println("num2=",num2)
 
 }
