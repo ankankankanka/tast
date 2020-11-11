942ca6465ff0bb39890dc8366510155b89fff887
ankankankanka <73925669+ankankankanka@users.noreply.github.com>
2020-11-11 23:10:47 +0800

    demo05
    demo05

--- main.go
+++ main.go
@@ -3,33 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	//要求，可以从控制台接收用户信息，【姓名，年龄，薪水，是否通过考试】
-	//方式1 fmt.Scanln()
-	//1先声明需要的变量
-	var name string
-	var age byte
-	var sal float32
-	var ispass bool
-	fmt.Println("请输入姓名")
-	fmt.Scanln(&name)
-	//当程序执行到fmt. scanln(&name)，程序会停止在这里，等待用户输入，并回车
+	//赋值运算符的使用演示
+	//var i int
+	//i = 10 //基本赋值
 
-	fmt.Println("请输入年龄")
-	fmt.Scanln(&name)
+	//有两个变量，a和b,要求将其进行交换，最终打印结果
+	//a = 9, b = 2 ==> a = 2 b = 9
+	a := 9
+	b := 2
+	fmt.Printf("交换前的情况是 a = %v, b=%v \n",a,b)
+	//定义一个临时变量
+	//t := a
+	//a := b
+	//b := t
+	fmt.Printf("交易后的情况是 a = %v ,b=%v \n",a,b)
 
-	fmt.Println("请输入薪水")
-	fmt.Scanln("&sal")
+	//符合贬值的操作
+	a += 17//等价 a = a +17
+	fmt.Println("a=",a)
 
-	fmt.Println("请输入是否通过考试")
-	fmt.Scanln("&ispass")
+	//var c int
+	//c = a + 3//赋值运算的执行顺序是从右向左
 
-	fmt.Printf("名字是%v \n 年龄是%v \n 是否通过考试%v\n",name,age,sal,ispass )
-
-	//方式2 fmt.scanf 可以按指定的格式输入
-	fmt.Println("请输入你的姓名，年龄，薪水，是否通过考试 ")
-	fmt.Sprintf("%s %d %f %t",&name,&age,&sal,&ispass)
-	fmt.Printf("名字是 %v \n 年龄是 %v \n 薪水是 %v \n 是否通过考试 %v \n " )
-
-
+	//赋值运算符的左边，只能是变量 右边可以是变量，表达式， 常量值
+	//表达式，任何有值都可以看做表达式
+	var d int
+	d = a
+	d = 8 + 2 * 8 // 的右边是表达式
+	d = 890 //890是常量
+	fmt.Println(d)
 
 }
