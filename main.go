44c7c4069d2e97e88a5a365d914dbb52b2b79f3b
ankankankanka <73925669+ankankankanka@users.noreply.github.com>
2020-11-11 23:07:03 +0800

    demo07
    demo07

--- main.go
+++ main.go
@@ -3,18 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	//golang的变量使用方式1
-	//第一种，指定变量类型，声明后若不赋值，使用默认值
-	//int 的默认值就是0
-	var i int
-	fmt.Println("i=",i)
-	//第二种，根据值自行判定变量类型（类型推导）
-	var num = 10.11
-	fmt.Println("num=", num )
-	//第三种，省略var，注意；=左侧的变量不应该时已经声明过的否则会导致编译错误
-	//：=的 ：不能省略，否则错误
-	name :="tom"
-	fmt.Println("name=",name)
+	//要求，可以从控制台接收用户信息，【姓名，年龄，薪水，是否通过考试】
+	//方式1 fmt.Scanln()
+	//1先声明需要的变量
+	var name string
+	var age byte
+	var sal float32
+	var ispass bool
+	fmt.Println("请输入姓名")
+	fmt.Scanln(&name)
+	//当程序执行到fmt. scanln(&name)，程序会停止在这里，等待用户输入，并回车
+
+	fmt.Println("请输入年龄")
+	fmt.Scanln(&name)
+
+	fmt.Println("请输入薪水")
+	fmt.Scanln("&sal")
+
+	fmt.Println("请输入是否通过考试")
+	fmt.Scanln("&ispass")
+
+	fmt.Printf("名字是%v \n 年龄是%v \n 是否通过考试%v\n",name,age,sal,ispass )
+
+	//方式2 fmt.scanf 可以按指定的格式输入
+	fmt.Println("请输入你的姓名，年龄，薪水，是否通过考试 ")
+	fmt.Sprintf("%s %d %f %t",&name,&age,&sal,&ispass)
+	fmt.Printf("名字是 %v \n 年龄是 %v \n 薪水是 %v \n 是否通过考试 %v \n " )
 
 
 
