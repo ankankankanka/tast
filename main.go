d73d0430d79e848f37f027b9e4741e1ed517da7a
ankankankanka <73925669+ankankankanka@users.noreply.github.com>
2020-11-11 23:11:23 +0800

    demo6
    demo6

--- main.go
+++ main.go
@@ -1,36 +1,24 @@
 package main
 
 import "fmt"
-
 func main() {
-	//赋值运算符的使用演示
-	//var i int
-	//i = 10 //基本赋值
+	//演示一把 & 和 * 的使用
 
-	//有两个变量，a和b,要求将其进行交换，最终打印结果
-	//a = 9, b = 2 ==> a = 2 b = 9
-	a := 9
-	b := 2
-	fmt.Printf("交换前的情况是 a = %v, b=%v \n",a,b)
-	//定义一个临时变量
-	//t := a
-	//a := b
-	//b := t
-	fmt.Printf("交易后的情况是 a = %v ,b=%v \n",a,b)
+	a := 100
+	fmt.Println("a 的地址=",&a)
 
-	//符合贬值的操作
-	a += 17//等价 a = a +17
-	fmt.Println("a=",a)
+	var ptr *int = &a
+	fmt.Println("ptr 指向的值是=",*ptr)
 
-	//var c int
-	//c = a + 3//赋值运算的执行顺序是从右向左
-
-	//赋值运算符的左边，只能是变量 右边可以是变量，表达式， 常量值
-	//表达式，任何有值都可以看做表达式
-	var d int
-	d = a
-	d = 8 + 2 * 8 // 的右边是表达式
-	d = 890 //890是常量
-	fmt.Println(d)
-
+	var n int
+	var i int = 10
+	var j int = 12
+	//传统的三元运算
+	//n = i > j ? i :j
+	if i > j {
+		n = i
+	} else {
+		n = j
+	}
+	fmt.Println("n=",n)
 }
