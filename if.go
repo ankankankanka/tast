9b69a46e21696c9cfcf69d3a2feba1dd205cc5f6
ankankankanka <73925669+ankankankanka@users.noreply.github.com>
2020-11-14 08:31:39 +0800

    if1
    双分支的基本使用

--- if.go
+++ if.go
@@ -1,29 +1,26 @@
-
 package main
 
 import "fmt"
 
 func main() {
-	//编写一个程序，可以输入人的年龄，如果该同志的年龄大于18岁则输出'你年龄大
-	//于18'，要对自己行为负责
+	//编写一个程序，可以输入人的年龄，如果该同志的年龄大于18岁，要对
+	//自己的行为负责，否则，输出"你的年龄不大这次放过你了"
 
-	//分析
-	//1，年龄 ==>var age int
-	//2,从控制台接收一个输入 fmt.Scanln(&age)
-	//3，if 判断
+	//思路分析
+	//1.年龄 ==> var age int
+	//2.fmt.scaln接收
+	//3.if ---else
 
+	//代码
 	var age int
 	fmt.Println("请输入年龄：")
 	fmt.Scanln(&age)
 
-	if age > 18 {
-		fmt.Println("你年龄大于18，要对自己的行为负责")
-	}//单分支基本操作
-
-	//golang支持在if中，直接定义一个变量比如下面
-	if age := 20; age > 18 {
-		fmt.Println("你年龄大于18，要对自己的行为负责！")
-	}
-
+	if age > 18{
+		fmt.Scanln("你年龄大于18")
+	}else {
+		fmt.Println("你的年龄不大这次放过你了")
+	}//if双分支基本操作
 
 }
+
